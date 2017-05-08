2f769b6eb912ca122f1ecf449079a0465eeb57b8
LeRoyce Pearson <minergeemili@gmail.com>
2017-05-08 10:43:33 -0500

    Format code with autopep8

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,20 +5,19 @@ import fresh_tomatoes
 
 # Our list of movies. The movies are just ones that I enjoy. ;)
 movies = [
-media.Movie("Oscar",
-    "https://www.youtube.com/watch?v=CMMtpC7t4s4",
-    "https://upload.wikimedia.org/wikipedia/en/3/31/Oscar1991poster.jpg"),
-media.Movie("The R.M.",
-    "https://www.youtube.com/watch?v=qGTkCLhM12o",
-    "https://upload.wikimedia.org/wikipedia/en/3/3c/The_R.M._poster.jpg"),
-media.Movie("Napoleon Dynamite",
-    "https://www.youtube.com/watch?v=ZHDi_AnqwN4",
-    "https://upload.wikimedia.org/wikipedia/en/8/87/Napoleon_dynamite_post.jpg"),
-media.Movie("The Lord of the Rings",
-    "https://www.youtube.com/watch?v=V75dMMIW2B4",
-    "https://upload.wikimedia.org/wikipedia/en/8/87/Ringstrilogyposter.jpg"),
+    media.Movie("Oscar",
+                "https://www.youtube.com/watch?v=CMMtpC7t4s4",
+                "https://upload.wikimedia.org/wikipedia/en/3/31/Oscar1991poster.jpg"),
+    media.Movie("The R.M.",
+                "https://www.youtube.com/watch?v=qGTkCLhM12o",
+                "https://upload.wikimedia.org/wikipedia/en/3/3c/The_R.M._poster.jpg"),
+    media.Movie("Napoleon Dynamite",
+                "https://www.youtube.com/watch?v=ZHDi_AnqwN4",
+                "https://upload.wikimedia.org/wikipedia/en/8/87/Napoleon_dynamite_post.jpg"),
+    media.Movie("The Lord of the Rings",
+                "https://www.youtube.com/watch?v=V75dMMIW2B4",
+                "https://upload.wikimedia.org/wikipedia/en/8/87/Ringstrilogyposter.jpg"),
 ]
 
 # Show the page in a browser
 fresh_tomatoes.open_movies_page(movies)
-
--- media.py
+++ media.py
@@ -1,8 +1,9 @@
 
-# A class to hold movie information. Used to build a page.
+
 class Movie():
+
+    # A class to hold movie information. Used to build a page.
     def __init__(self, title, trailer, poster):
         self.title = title
         self.trailer_youtube_url = trailer
         self.poster_image_url = poster
-
